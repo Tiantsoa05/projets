d3c447f81e0b70577f58169ff3492ff2b4b8d822
Jossuc4 <larryjossuc4@gmail.com>
2024-03-20 22:45:42 +0300

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from 'express'
 import app from './routes/route.js'
 const server = express() 
 
+
+// server.use(express.json()): Pour transformer toutes données depuis le client en objet json 
+//Ilaina amle modification ;) 
+server.use(express.json())
+
 server.use('/employers',app)
 
 
