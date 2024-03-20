6c3ad8ba68e881382221970f85f66f67438f8660
Jossuc4 <larryjossuc4@gmail.com>
2024-03-20 22:37:50 +0300

    je t'aime aussi

--- routes/route.js
+++ routes/route.js
@@ -7,8 +7,30 @@ const app = express()
 
 app.get('/all',function(requete, reponse){
 
-const data = database.query("SELECT * FROM ouvrier")
-reponse.status(200).end(JSON.stringify(data))
+database.query("SELECT * FROM ouvriers",function(error,data){
+    if(error) throw error
+    reponse.status(200).end(JSON.stringify(data))
+})
+
+})
+
+//Suppresion d'ouvrier
+
+app.delete('/suppr/:id',function(request,response){
+
+    //chercher l'id dans l'url paramétrée 
+    const {id} = request.params
+
+    //etablir la requete de suppression
+    database.query("DELETE FROM ouvriers WHERE id=?",[id],function(error,results){
+        if(error)throw error
+        else{
+            response.status(200).end(JSON.stringify({
+                message: "suppression réussie"
+            }))
+        }
+    })
+
 })
 
 export default app
