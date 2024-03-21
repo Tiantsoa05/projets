7a1a84f984914ce4e21b4c80c0ea8c5119a41f87
Tiantsoa05 <tiantsoahortencia@gmail.com>
2024-03-21 17:49:17 +0300

    frontend

--- frontend/FrontendNode/src/component/FormulaireModif.jsx
+++ frontend/FrontendNode/src/component/FormulaireModif.jsx
@@ -5,12 +5,18 @@ export default function FormulaireModif(afficher, infoOuvrier) {
     const [detailOuvrier, setInfoOuvrier] = useState(infoOuvrier)
     const { num_ouvrier, nom_ouvrier, sexe_ouvrier, nbr_jours, taux_journalier } = detailOuvrier
 
+    const [num,setNum] = useState("")
+    const [nom,setNom] = useState("")
+    const [sexe,setSexe] = useState("")
+    const [nbr,setNbr] = useState(0)
+    const [taux,setTaux] = useState(0)
+
     return <div className="blur-container">
 
         <div className="form">
             <form>
-                <div><input type="text" name="num_ouvrier" value={num_ouvrier} /></div>
-                <div><input type="text" name="nom_ouvrier" value={nom_ouvrier} /></div>
+                <div><input type="text" name="num_ouvrier" value={num_ouvrier} onChange={setNum(num_ouvrier)}/></div>
+                <div><input type="text" name="nom_ouvrier" value={nom_ouvrier} onChange={setNom(nom_ouvrier)}/></div>
                 <div>
                     <input type="radio" name="sexe_ouvrier" value="" />
                     <input type="radio" name="sexe_ouvrier" value="" />
